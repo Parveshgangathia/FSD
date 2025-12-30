7691e1baa78e4dbd0916e67c38ae70278c30f133
unknown <pk8352082260@gmail.com>
2025-12-30 23:58:58 +0530

    Day_40 plus First capstone project

---
+++ 1stCapstoneProject/project_01/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      App is rendering
+    </div>
+  );
+}
+
+export default App;
---
+++ 1stCapstoneProject/project_01/src/components/EmptyState.jsx
@@ -0,0 +1,9 @@
+function EmptyState() {
+  return (
+    <div className="text-center text-gray-400 py-10">
+      No tasks yet. Add one above 👆
+    </div>
+  );
+}
+
+export default EmptyState;
